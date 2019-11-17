4b706868174c2faae907a6eb60e42783738acd9b
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2019-11-17 13:15:01 +0700

    Fix (edit pegawai): wrong $_POST name, wrong id pegawai, wrong SQL

--- pelaksanaadmkeuangan/upegawai.php
+++ pelaksanaadmkeuangan/upegawai.php
@@ -5,21 +5,25 @@
   include("../libfunc.php");
   $link=koneksidb();
   if(($_SESSION['logske']==true) && ($_SESSION['userske']!="")){
-    $nama=$_POST['nama'];
-	$sql4="UPDATE nama SET nama='$nama' WHERE nama='$nama'";
-    $jabatan=$_POST['jabatan'];
-    $gaji=$_POST['gaji'];
-	$sql="UPDATE gaji SET gaji='$gaji' WHERE gaji='$gaji'";
-    $res1=mysqli_query($link,$sql4);
-	
-	
-	$idpeg="pp".($data4['nilai']+1);
-                        $sql7="INSERT INTO pegawai VALUES ('$idpeg','$nama','$jabatan')";
-                        $res7=mysqli_query($link,$sql7);
+	$idpeg=$_POST['idpeg'];
+
+    $nama=$_POST['namabaru'];
+	$sql4="UPDATE pegawai SET nama='$nama' WHERE id_pegawai='$idpeg'";
+    $res4=mysqli_query($link,$sql4);
+    $jabatan=$_POST['jabatanbaru'];
+	$sql5="UPDATE pegawai SET jabatan='$jabatan' WHERE id_pegawai='$idpeg'";
+    $res5=mysqli_query($link,$sql5);
+    $gaji=$_POST['gajibaru'];
+	$sql="UPDATE gaji SET gaji='$gaji' WHERE id_pegawai='$idpeg'";
+    $res1=mysqli_query($link,$sql);
+    echo mysqli_error($link);
+
+                        //$sql7="INSERT INTO pegawai VALUES ('$idpeg','$nama','$jabatan')";
+                        //$res7=mysqli_query($link,$sql7);
 						
-		$sql2="INSERT INTO gaji VALUES ('$idpeg','$gaji')";
-                        $res2=mysqli_query($link,$sql2);
-                        if($res7 && $res2)			
+		//$sql2="INSERT INTO gaji VALUES ('$idpeg','$gaji')";
+                        //$res2=mysqli_query($link,$sql2);
+                        if($res4 && $res5 && $res1)			
 						
 						
 	
